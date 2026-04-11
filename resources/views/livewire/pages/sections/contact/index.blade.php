679eb13a881d8aef9857bfe8a555476dd7519e03
gabrielpereira1603 <gabriel_ap02@yahoo.com>
2026-04-10 23:50:45 -0300

    ALTERANDO YML

--- resources/views/livewire/pages/sections/contact/index.blade.php
+++ resources/views/livewire/pages/sections/contact/index.blade.php
@@ -62,6 +62,7 @@
                     </div>
                 </div>
 
+
                 <!-- Redes sociais -->
                 <div class="mt-12">
                     <div class="flex items-center gap-4 mb-6">
